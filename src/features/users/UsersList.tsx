cd1273b3ab89343bc308befab80760bdf48cff81
bryanonyc <bryanonyc@yahoo.com>
2024-09-30 09:55:09 -0400

    Add search filtering to the user list table

--- src/features/users/UsersList.tsx
+++ src/features/users/UsersList.tsx
@@ -1,22 +1,110 @@
-import { Alert, Button, Checkbox, Empty, Table, TableProps } from 'antd';
+import { useRef, useState } from 'react';
+import type { InputRef, TableColumnType } from 'antd';
+import { Alert, Button, Checkbox, Empty, Input, Space, Table, TableProps } from 'antd';
 import { useGetUsersQuery } from './usersApiSlice'
 import { User } from '../../models/user';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FilterDropdownProps } from 'antd/es/table/interface';
+import { SearchOutlined } from '@ant-design/icons';
+
+type DataIndex = keyof User;
 
 const UsersList = () => {
-    const navigate = useNavigate();
     const { data: queryData, isSuccess, isError, error } = useGetUsersQuery();
+
+    const [, setSearchText] = useState('');
+    const [, setSearchedColumn] = useState('');
+    const searchInput = useRef<InputRef>(null);
+
+    const handleSearch = (
+        selectedKeys: string[],
+        confirm: FilterDropdownProps['confirm'],
+        dataIndex: DataIndex,
+    ) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (
+        clearFilters: () => void,
+        confirm: FilterDropdownProps['confirm']
+    ) => {
+        clearFilters();
+        setSearchText('');
+        confirm();
+    };
+
+
+    const getColumnSearchProps = (dataIndex: DataIndex): TableColumnType<User> => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                    style={{ marginBottom: 8, display: 'block' }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters, confirm)}
+                        size="small"
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        Close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered: boolean) => (
+          <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
+        ),
+        onFilter: (value, record) =>
+          record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes((value as string).toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+          if (visible) {
+            setTimeout(() => searchInput.current?.select(), 100);
+          }
+        },
+        render: (text) =>
+          (
+            text
+          ),
+      });
 
     const columns: TableProps<User>['columns'] = [
         {
             title: 'Name',
             dataIndex: 'name',
             key: 'name',
+            ...getColumnSearchProps('name'),
         },
         {
             title: 'Email',
             dataIndex: 'email',
             key: 'email',
+            ...getColumnSearchProps('email'),
         },
         {
             title: 'Admin',
@@ -38,7 +126,7 @@ const UsersList = () => {
             render: (_, record) => (
                 <Link to={`${record.id}/edit`}>Edit</Link>
             ),
-          },
+        },
     ];
 
     let tableData: User[] = [];
